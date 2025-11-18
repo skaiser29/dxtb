ceaa82b4914725f61fe4d13d1dc0ad166d7e02ba
Scott Kaiser <scottirakaiser@gmail.com>
2025-11-18 13:25:12 -0500

    Added init tooling so QMMM attributes are seen by pip

---
+++ src/dxtb/_src/components/interactions/qmmm/__init__.py
@@ -0,0 +1,15 @@
+from .gcem import GCEM, LABEL_GCEM, new_gcem
+from .gcem_pbc import GCEMPBC, LABEL_GCEMPBC, new_gcempbc
+from .pcem import PCEM, LABEL_PCEM, new_pcem
+
+__all__ = [
+    "GCEM",
+    "LABEL_GCEM",
+    "new_gcem",
+    "GCEMPBC",
+    "LABEL_GCEMPBC",
+    "new_gcempbc",
+    "PCEM",
+    "LABEL_PCEM",
+    "new_pcem",
+]
